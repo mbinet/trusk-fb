876e346d2ab049d72da3315d1c8be5d5d01f95c9
maxime <paxman2@hotmail.fr>
2016-11-15 14:10:36 +0100

    Added pagination

--- src/app/components/Root.js
+++ src/app/components/Root.js
@@ -1,23 +1,58 @@
 import React from "react";
 import Request from "superagent";
 import _ from "lodash";
-import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
 import FontAwesome from 'react-fontawesome';
+import { browserHistory, Link } from "react-router";
 
 export class Root extends React.Component {
 
     constructor() {
         super();
-        this.state = {
-        };
+        this.state = {};
     }
 
-    componentDidMount() {
+    componentWillMount() {
+        if (this.props.params.next) {
+            this.getRatings(this.props.params.next);
+        }
+        else {
+            // no param in the url
+            this.getRatings("");
+        }
+    }
+
+    // Gets the new url and sends the param "next" to request next ratings
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.params.next) {
+            this.getRatings(nextProps.params.next);
+        }
+        else {
+            this.getRatings("");
+        }
+    }
+
+    componentDidUpdate() {
+        // This is for scrolling back to the top after every page change.
+        window.scrollTo(0, 0)
+    }
+
+    getRatings(next) {
         var fb_page_access_token = "CAAbiZCqFSoHsBAC2ZBWiDTJ9q9q3gPfN1RxEjUGK7LcO80ZBQ4i804fQ5X6aZAmYa6tpHNJkuyI8lXLKzfZCgLNOaJNTi4GqWEKZAn4rZBaWrvnAkUJsLoWpwlktDhSUKMMDBjyfgrgGyjK5TzbmvZBAugvwReVzh7ZC3nwANFSJG1IrsI2aFe5upkeiv8sZCO4ZAAZD";
-        var url = "https://graph.facebook.com/v2.5/truskapp/ratings?access_token=" + fb_page_access_token + "&format=json&limit=200";
+        var url = "https://graph.facebook.com/v2.5/truskapp/ratings?access_token=" + fb_page_access_token + "&format=json&limit=25";
+
+        // adds an "after" param to the url
+        if (next != "") {
+            url += "&after=" + next;
+        }
         Request.get(url).then((response) => {
+            console.log(response);
             this.setState({
-                ratings: response.body.data
+                // store all the ratings
+                ratings: response.body.data,
+                // store the cursor to the next load of ratings
+                after: response.body.paging.cursors.after
             });
         });
     }
@@ -43,11 +78,17 @@ export class Root extends React.Component {
         return [day, month].join(' ');
     }
 
+    nextPage() {
+        browserHistory.push("/?url=" + this.state.next);
+    }
+
     render() {
         var star = <FontAwesome className="fa fa-star" name="" style={{color: '#5890FF'}}/>;
+        // this will loop on the ratings
         var ratings = _.map(this.state.ratings, (rating) => {
             return(
                 <div className="col-sm-12" key={rating.reviewer.id}>
+                    {/* displays a nice card */}
                     <Card style={{marginBottom: '30px'}}>
                         <CardHeader
                             title={<div>{rating.reviewer.name} {rating.rating} {star} </div>}
@@ -62,9 +103,19 @@ export class Root extends React.Component {
             )
         });
         return (
-            <div>
+            <div className="container">
+                <Link to={"/"}>
+                    <h1>Trusk Facebook Reviews</h1>
+                </Link>
+                <hr />
+                <br />
                 <div className="row">
                     {ratings}
+                </div>
+                <div className="text-center">
+                    <Link to={"/" + this.state.after}>
+                        <FlatButton label="Next"/>
+                    </Link>
                 </div>
             </div>
         )
--- src/app/index.js
+++ src/app/index.js
@@ -3,6 +3,7 @@ import { render } from "react-dom";
 import {Root} from "./components/Root";
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Router, Route, browserHistory, IndexRoute} from "react-router";
 
 injectTapEventPlugin();
 
@@ -10,12 +11,16 @@ class App extends  React.Component {
     render() {
         return (
             <MuiThemeProvider>
-                <div className="container">
-                    <h1>Trusk Ratings</h1>
-                    <hr />
-                    <br />
-                    <Root/>
-                </div>
+                <Router history={browserHistory}>
+                    <Route path={"/(:next)"} component={Root}>
+                    {/*<div className="container">*/}
+                        {/*<h1>Trusk Ratings</h1>*/}
+                        {/*<hr />*/}
+                        {/*<br />*/}
+                        {/*<Root/>*/}
+                    {/*</div>*/}
+                    </Route>
+                </Router>
             </MuiThemeProvider>
         )
     }
